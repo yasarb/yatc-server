0afb3ffa68c95f98a5255c2c4c2899151fe359dd
yasar budulgan <yb.budulgan@gmail.com>
2019-07-29 22:29:52 +0300

    :fire: Remove unnecessary endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,16 +13,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Post('write')
-  write(@Body() body) {
-    this.redis.getClient().incrby('testkey', 1);
-    return 'hello';
-  }
-
-  @Get('read')
-  read() {
-    const value = this.redis.getClient().get('testkey');
-    return value;
-  }
 }
