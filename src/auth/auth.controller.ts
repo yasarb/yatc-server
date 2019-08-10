efce6de02b097734eb9bcd8d1cff466fe432f0de
yasar budulgan <yb.budulgan@gmail.com>
2019-08-10 19:34:38 +0300

    :fire: Remove unused imports and comments

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,6 @@ import {
   Request,
   Headers,
   Body,
-  BadRequestException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -1,33 +1,7 @@
 import { Strategy } from 'passport-local';
-import {
-  Injectable,
-  UnauthorizedException,
-  BadRequestException,
-} from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 import { use } from 'passport';
-
-/*
-@Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
-    super();
-  }
-
-  async validate(username: string, password: string): Promise<any> {
-    console.log(`username: ${username}`);
-    const user = await this.authService.validateUser(username, password);
-
-    if (!user) {
-      console.log(`username: ${username}`);
-      throw new UnauthorizedException();
-    }
-
-    return user;
-  }
-}
-*/
 
 @Injectable()
 export class LocalStrategy {
