626b4be424fc660d973257fb6da1c600225610ac
yasar budulgan <yb.budulgan@gmail.com>
2019-08-23 23:46:46 +0300

    Add validators to dto classes

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,3 +1,8 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
 export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(150)
   readonly text: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,28 @@
+import {
+  IsNotEmpty,
+  IsAlphanumeric,
+  MaxLength,
+  IsEmail,
+  IsISO31661Alpha2,
+  Length,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsAlphanumeric()
+  @MaxLength(32)
   readonly username: string;
+
+  @IsNotEmpty()
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(128)
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsISO31661Alpha2()
+  @Length(2)
   readonly lang: string;
 }
