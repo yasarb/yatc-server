3b2a6416c23acf4bf84ab0a98252acc6d2a4ca0e
yasar budulgan <yb.budulgan@gmail.com>
2019-08-25 23:50:45 +0300

    Add mocks for auth service

--- src/auth/test/mocks.ts
+++ src/auth/test/mocks.ts
@@ -1,3 +1,5 @@
+import { User } from '../../users/users.service';
+
 /* tslint:disable:no-empty */
 export const mockAuthService = {
   validateUser() {},
@@ -5,4 +7,34 @@ export const mockAuthService = {
   signup() {},
   signout(authToken: string) {},
 };
+
+export const mockUsersService = {
+  findByUsername(username: string): Promise<User> {
+    return new Promise(() => {});
+  },
+  save(entity: User): Promise<User> {
+    return new Promise(() => {});
+  },
+};
+
+export const mockJwtService = {
+  sign(payload: string) {},
+};
+
+export const mockRedisService = {};
+
+export const mockUser: User = {
+  userId: 1,
+  username: 'mockuser',
+  password: 'mockpassword',
+  email: 'test@mail.com',
+  profilePhotoUrl: '',
+  profileBannerUrl: '',
+  registeredAt: '',
+  isAdmin: false,
+  isActive: true,
+  isVerified: false,
+  isPrivate: false,
+};
+
 /* tslint:enable:no-empty */
