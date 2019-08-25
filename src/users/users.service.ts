635b19a70973562945cf3ae393d7de9d62527d61
yasar budulgan <yb.budulgan@gmail.com>
2019-08-25 23:53:31 +0300

    Use user repository in user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,38 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './user.entity';
+import { getRepository } from 'typeorm';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
-  async findUserByUsername(username: string): Promise<User | undefined> {
-    return User.findOne({ username }).then(user => {
+  async findByUsername(username: string): Promise<User | undefined> {
+    return this.findOne({ username }).then(user => {
       if (user) {
         return user;
       } else {
-        return undefined;
+        throw new NotFoundException();
       }
     });
   }
 
   async findUserById(userId: number): Promise<User> {
-    return User.findOne({ userId }).then(user => {
+    return this.findOne({ userId }).then(user => {
       if (user) {
         return user;
       } else {
         throw new NotFoundException(`User not found with given id: ${userId}`);
       }
     });
+  }
+
+  async save(entity: User): Promise<User> {
+    const userRepository = getRepository(User);
+    return userRepository.save(entity);
+  }
+
+  private async findOne(filter): Promise<User | undefined> {
+    const userRepository = getRepository(User);
+    return userRepository.findOne(filter);
   }
 }
